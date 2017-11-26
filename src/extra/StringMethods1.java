934e56be230053e06c14945a4a61a38dc043ab51
DylanArnold101 <league@WTS22.attlocal.net>
2017-11-26 11:33:38 -0800

    sD>kj
    strings modules

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -2,30 +2,38 @@
 //    Level 0
 package extra;
 
-
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String flavvy = "oyyvyh";
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+
+		System.out.println(flavvy.charAt(2));
+		// HINT: .charAt
 		// 3. Print the length of your String to the console.
-		//    HINT: .length
+		// HINT: .length
 
+		System.out.println(flavvy.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
+		// HINT: use .length() to determine how many loops
 
-		
+		for (int i = 0; i < flavvy.length(); i++) {
+			System.out.print(flavvy.charAt(i));
+
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+
+		char letter = 'h';
+		for (int i = 0; i < flavvy.length(); i++) {
+			if (flavvy.charAt(i) == letter) {
+				System.out.println(" letter h is located at " + i);
+			}
+		}
+		// what position/index in the String the char is located.
+		// Print the char's position to the console.
+		// EXAMPLE: if your string is "abc" and you are searching
+		// for char 'b', then print "b is at index 1"
+
 	}
 }
-
-
-
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -4,22 +4,23 @@ package extra;
 
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it with
-		//    random upper and lower case characters.
-
+		// random upper and lower case characters.
+		String r = "hIvVVVVVjbVjEbolaJamagsaeg";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(r.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(r.toLowerCase());
 		// 4. Print the first 3 char's of your String
-		//    HINT: .substring(start,end)
-		
+		System.out.println(r.substring(0, 2));
+		// HINT: .substring(start,end)
+
 		// 5. Print a single char somewhere in the middle of your String
-		//    REMINDER: char's in string start at index 0
-		
+		// REMINDER: char's in string start at index 0
+		System.out.println(r.charAt(10));
 		// 6. BONUS -- print the LAST 3 char's of your string using
-		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(r.substring((r.length() - 3), r.length()));
+		// .length() to determine WHERE the last 3 char's are located.
 	}
 }
-
